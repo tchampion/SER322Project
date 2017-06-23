ac4c7a6440b9dcdf6e1e47d52928bcdbf92fbb57
hboughou <hboughou@asu.edu>
2017-06-23 15:38:01 -0400

    Fixed discrepancy between attribute names

--- db/mySQL.sql
+++ db/mySQL.sql
@@ -19,14 +19,14 @@ CREATE TABLE users(
     email VARCHAR(50) NOT NULL,
     pass VARCHAR(30) NOT NULL);
 INSERT INTO users
-	(`User ID`, `First Name`, `Last Name`, `Email`, `Password`)
+	(`user_id`, `firstName`, `lastName`, `email`, `pass`)
 VALUES
-	(1, 'Terin', 'Champion', 'terinchampion@gmail.com', 'shesthechamp'),
-	(2, 'Nergal', 'Givarkes', 'ngivarke@asu.edu', 'nero'),
-	(3, 'Hajar', 'Boughoula', 'hboughou@asu.edu', 'crunchingcode'),
-	(4, 'Donald', 'Trump', 'cheeto@whitehouse.gov', 'smallhands'),
-	(5, 'Cookie', 'Monster', 'cookie@monster.inc', 'chochip'),
-	(6, 'Drake', 'Graham', 'drake@narcissism.org', 'foodbling');
+	('Terin', 'Champion', 'terinchampion@gmail.com', 'shesthechamp'),
+	('Nergal', 'Givarkes', 'ngivarke@asu.edu', 'nero'),
+	('Hajar', 'Boughoula', 'hboughou@asu.edu', 'crunchingcode'),
+	('Donald', 'Trump', 'cheeto@whitehouse.gov', 'smallhands'),
+	('Cookie', 'Monster', 'cookie@monster.inc', 'chochip'),
+	('Drake', 'Graham', 'drake@narcissism.org', 'foodbling');
     
 CREATE TABLE recipe(
     recipe_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -35,11 +35,11 @@ CREATE TABLE recipe(
     servings VARCHAR(20),
     likes INT DEFAULT 0);
 INSERT INTO recipe
-	(`Recipe ID`, `Title`, `Instructions`, `Servings`, `Likes`)
+	(`recipe_id`, `title`, `instructions`, `servings`, `likes`)
 VALUES
-	(1, 'Chocolate Chip Cookies', '1. Buy chocolate chip cookies' + CHAR(13) + '2. Eat them', '12', 2),
-	(2, 'Vegan Quiche', '1. Get vegan ingredients' + CHAR(13) + '2. Mix ingredients' + CHAR(13) + '3. bake for 30 minutes', '6', 3),
-	(3, 'Berries Smoothie', '1. Get mixed berries' + CHAR(13) + '2. Add banana and yogurt ' + CHAR(13) + '3. Mix in food processor' + CHAR(13) + '4. Serve chilled', '2', 6);
+	('Chocolate Chip Cookies', '1. Buy chocolate chip cookies' + CHAR(13) + '2. Eat them', '12', 2),
+	('Vegan Quiche', '1. Get vegan ingredients' + CHAR(13) + '2. Mix ingredients' + CHAR(13) + '3. bake for 30 minutes', '6', 3),
+	('Berries Smoothie', '1. Get mixed berries' + CHAR(13) + '2. Add banana and yogurt ' + CHAR(13) + '3. Mix in food processor' + CHAR(13) + '4. Serve chilled', '2', 6);
     
 CREATE TABLE item(
     item_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -48,21 +48,21 @@ CREATE TABLE item(
     description TEXT,
     amount VARCHAR(20));
 INSERT INTO item
-	(`Item ID`, `Brand`, `Item Name`, `Description`, `Amount`)
+	(`item_id`, `brand`, `item_name`, `description`, `amount`)
 VALUES
-	(1, 'Hershey's', 'Chocolate Chips', '', '12 oz'),
-	(2, 'Land O'Lakes', 'Butter', '', '4 oz'),
-	(3, 'Pillsbury', 'Flour', '', '16 oz'),
-	(4, 'Driscoll's', 'Blueberry', '', ''),
-	(5, 'Chiquita', 'Banana', '', '1 piece'),
-	(6, 'Yoplait', 'Strawberry Yogurt', '', '6 oz');
+	('Hershey's', 'Chocolate Chips', '', '12 oz'),
+	('Land O'Lakes', 'Butter', '', '4 oz'),
+	('Pillsbury', 'Flour', '', '16 oz'),
+	('Driscoll's', 'Blueberry', '', ''),
+	('Chiquita', 'Banana', '', '1 piece'),
+	('Yoplait', 'Strawberry Yogurt', '', '6 oz');
 
 CREATE TABLE user_recipe(
     user_id INT UNSIGNED NOT NULL REFERENCES users(user_id),
     recipe_id INT UNSIGNED NOT NULL REFERENCES recipe(recipe_id),
     PRIMARY KEY(user_id, recipe_id));
 INSERT INTO user_recipe
-	(`User ID`, `Recipe ID`)
+	(`user_id`, `recipe_id`)
 VALUES
 	(1, 1),
 	(1, 2),
@@ -87,7 +87,7 @@ CREATE TABLE pantry(
     user_id INT UNSIGNED NOT NULL REFERENCES users(user_id),
     PRIMARY KEY(pantry_id, user_id));
 INSERT INTO pantry
-	(`Pantry ID`, `User ID`)
+	(`pantry_id`, `user_id`)
 VALUES
 	(1, 1),
 	(2, 2),
@@ -102,7 +102,7 @@ CREATE TABLE pantry_item(
     amount INT UNSIGNED NOT NULL,
     PRIMARY KEY(pantry_id, item_id));
 INSERT INTO pantry_item
-	(`Pantry ID`, `Item ID`, `Amount`)
+	(`pantry_id`, `item_id`, `amount`)
 VALUES
 	(1, 1, 1),
 	(1, 2, 2),
@@ -122,7 +122,7 @@ CREATE TABLE shopping_list(
     user_id INT UNSIGNED NOT NULL REFERENCES users(user_id),
     PRIMARY KEY(shopping_list_id, user_id));
 INSERT INTO shopping_list
-	(`Shopping List ID`, `User ID`)
+	(`shopping_list_id`, `user_id`)
 VALUES
 	(1, 1),
 	(2, 2),
@@ -137,7 +137,7 @@ CREATE TABLE shopping_list_item(
     amount INT UNSIGNED NOT NULL,
     PRIMARY KEY(shopping_list_id, item_id));
 INSERT INTO shopping_list_item
-	(`Shopping List ID`, `Item ID`, `Amount`)
+	(`shopping_list_id`, `item_id`, `amount`)
 VALUES
 	(1, 2, 2),
 	(1, 6, 2),
